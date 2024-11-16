88a8e5a43205edc98ec9fc415b7cce6e6ad8d169
zhifanjia <allenjia98@gmail.com>
2024-11-16 16:25:21 -0500

    db connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL )
+    .then( () =>
+    {console.log('Connected to MongoDB')}
+)
 
 const app = express();
 
