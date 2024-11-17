66db15fff0d653ddbd57e4edc2c22bc368981102
zhifanjia <allenjia98@gmail.com>
2024-11-17 10:04:15 -0500

    create middleware to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: 'All fields are required' });
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcrypt.hashSync(password, 12);
@@ -15,6 +16,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         return res.status(200).json({ message: 'User created successfully' });
     } catch (error) {
-        return res.status(500).json({ message: 'An error occurred' });
+        next(error);
     }
 }
--- api/index.js
+++ api/index.js
@@ -17,6 +17,16 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((req, res, err, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'An error occurred';
+    res.status(statusCode).json({ 
+        success: false,
+        statusCode,
+        message 
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
