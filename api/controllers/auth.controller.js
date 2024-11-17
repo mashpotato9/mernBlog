b54789f34d7aa208d606e08952c5f9a3990565f4
zhifanjia <allenjia98@gmail.com>
2024-11-17 18:15:11 -0500

    create login api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -15,6 +16,40 @@ export const register = async (req, res, next) => {
     try{
         await newUser.save();
         return res.status(200).json({ message: 'User created successfully' });
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const login = async (req, res, next) => {
+    const {username, password} = req.body;
+
+    if (!username || !password || username === '' || password === '') {
+        return next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return next(errorHandler(404, 'User not found'));
+        }
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+            return next(errorHandler(400, 'Invalid credentials'));
+        }
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        )
+
+        const { password: userPassword, ...userInfo } = user._doc;
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+            //secure: process.env.NODE_ENV === 'production',
+            maxAge: 3600000
+        }).json(userInfo);
     } catch (error) {
         next(error);
     }
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
-import SignIn from './pages/SignIn.jsx'
 import Register from './pages/Register.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import Projects from './pages/Projects.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Login from './pages/Login.jsx'
 
 export default function App() {
   return (
@@ -15,7 +15,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
