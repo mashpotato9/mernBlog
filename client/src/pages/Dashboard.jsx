c7a75d8e27b74934fd6312549b3f4c06b1fd9447
zhifanjia <allenjia98@gmail.com>
2024-11-19 15:13:11 -0500

    complete sidebar of profile

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from 'react';
+import {useLocation} from 'react-router-dom';
+import DashSidebar from '../components/DashSidebar.jsx';
+import DashProfile from '../components/DashProfile.jsx';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <div>dashBoard</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+      {/* sidebar */}
+        <DashSidebar />
+      </div>
+      <div>
+        {/* main content */}
+        {tab === 'profile' && <DashProfile />}
+      </div>
+    </div>
   )
 }
