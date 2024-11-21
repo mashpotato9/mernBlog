97950d4df626e9e07585fec1346a3a6e8a9317c9
zhifanjia <allenjia98@gmail.com>
2024-11-21 14:05:44 -0500

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -47,3 +47,15 @@ export const updateUser = async (req, res, next) => {
         next(error);
     }
 };
+
+export const deleteUser = async (req, res, next) => {
+    if (req.user.userId !== req.params.id) {
+        return next(errorHandler(403, 'Unauthorized to update user'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'User deleted successfully'});
+    } catch (error) {
+        next(error);
+    }
+}
