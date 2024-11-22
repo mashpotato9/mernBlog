090e8c5de92de86482f2a8ae3e74b11b55f2f4b1
zhifanjia <allenjia98@gmail.com>
2024-11-22 13:58:42 -0500

    complete upload post img functionality

--- client/src/pages/NewPost.jsx
+++ client/src/pages/NewPost.jsx
@@ -1,8 +1,54 @@
 import { TextInput, Select, FileInput, Button } from 'flowbite-react';
+import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase.js';
+
 
 export default function NewPost() {
+    const [file, setFile] = useState(null);
+    const [imgUploadProgress, setImgUploadProgress] = useState(null);
+    const [imgUploadError, setImgUploadError] = useState(null);
+    const [form, setForm] = useState({});
+
+    const handleUploadImg = async () => {
+        try{
+            if(!file){
+                setImgUploadError('Please select an image');
+                return;
+            }
+            setImgUploadError(null);
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                'state_changed', 
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setImgUploadProgress(progress.toFixed(0));
+                }, 
+                (error) => {
+                    setImgUploadError(error.message);
+                    setImgUploadProgress(null);
+                }, 
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        setImgUploadProgress(null);
+                        setImgUploadError(null);
+                        setForm({...form, image: downloadURL});
+                    });
+                }
+            );
+        } catch (error) {
+            setImgUploadError('An error occurred');
+            setImgUploadProgress(null);
+            console.log(error);
+        }
+
+    }
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
         <h1 className="text-center text-3xl my-7 font-semibold">New Post</h1>
@@ -18,8 +64,8 @@ export default function NewPost() {
                 </Select>
             </div>
             <div className='flex gap-4 items-center justify-between border-4 border-blue-200 border-dotted p-3'>
-                <FileInput type='file' accept='image/*' />
-                <Button type='button' gradientDuoTone='tealToLime' size='sm' outline>Upload Image</Button>
+                <FileInput type='file' accept='image/*' onChange={(e) => setFile(e.target.files[0])} />
+                <Button type='button' gradientDuoTone='tealToLime' size='sm' outline onClick={handleUploadImg} >Upload Image</Button>
             </div>
             <ReactQuill theme='snow' placeholder='Write Something' className='h-72 mb-12'/>
             <Button type='submit' gradientDuoTone='tealToLime'>Publish</Button>
