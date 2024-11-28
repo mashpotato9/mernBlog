22166744a05abe95d33adcf1b1c4c1abc0c11c64
zhifanjia <allenjia98@gmail.com>
2024-11-28 16:11:50 -0500

    complete homepage

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -3,15 +3,18 @@ import { Button } from "flowbite-react";
 
 export default function CallToAction() {
   return (
-    <div className="flex flex-col sm:flex-row border border-green-200 justify-center items-center rounded-tl-xl rounded-br-xl p-3 ">
+    <div className="flex flex-col sm:flex-row border border-green-200 justify-center items-center rounded-tl-xl rounded-br-xl p-3">
         <div className="flex-1 justify-center flex flex-col">
-            <h2 className="text-2xl font-serif">Embracing AI in Advertising</h2>
-            <p className="text-sm mt-3 my-2">AI is revolutionizing the advertising industry. Learn how you can leverage AI to improve your ad campaigns.</p>
-            <Button gradientDuoTone='purpleToBlue'className="mt-3 text-gray-100 px-3 py-1 rounded-md ">
-              <a href="https://www.semrush.com/blog/ai-advertising/" target="_blank" >
-                Learn More
-              </a>
-            </Button>
+            <h2 className="text-2xl font-serif">Check Out Source Code</h2>
+            <p className="text-sm mt-3 my-2">You can find the source code of this website by clicking the button</p>
+            <div className="w-fit">
+              <Button gradientDuoTone='purpleToBlue'className="mt-3 text-gray-100 px-3 py-1 rounded-md ">
+                <a href="https://github.com/mashpotato9/mernBlog" target="_blank" >
+                  Learn More
+                </a>
+              </Button>
+            </div>
+
         </div>
         <div className="ml-10">
             <img src="https://static.semrush.com/blog/uploads/files/c2/0d/c20d6af1070cd45205f7c7ab4a92f049/embracing-ai-in-advertising.svg" alt="" />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,59 @@
+import { Link } from 'react-router-dom'
+import CallToAction from '../components/CallToAction.jsx'
+import { useEffect, useState } from 'react'
+import apiRequest from '../../lib/apiRequest.js'
+import PostCard from '../components/PostCard.jsx'
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await apiRequest.get('/post?limit=6');
+      if (res.status === 200) {
+        setPosts(res.data.posts);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+
   return (
-    <div>Home</div>
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold lg:text-6xl ">Welcome to my blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          fringilla, nunc at ultrices posuere, nunc libero ultricies diam, nec
+          tempus odio purus nec ipsum. Donec auctor, tortor nec lacinia
+          elementum, elit metus tincidunt
+        </p>
+        <Link to='/search' className='text-xs sm:text-sm text-blue-300 font-bold hover:underline '>
+        View all posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-400 max-w-6xl mx-auto'>
+        <CallToAction />
+      </div>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {
+          posts && posts.length > 0 && (
+            <div className='p-3 max-w-6xl mx-auto'>
+              <h2 className='text-2xl font-semibold'>Recent Posts</h2>
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+                {posts.map((post) => (
+                  <PostCard key={post._id} post={post} />
+                ))}
+              </div>
+            </div>
+          )
+        }
+      </div>
+      <div className='flex justify-center pb-5'>
+        <Link to={'/search'} className='text-lg text-blue-500 hover:underline'>
+          View All Posts
+        </Link>
+      </div>
+    </div>
   )
 }
