cf223aa531fe154e2fda9caaea846cb4cc3d8887
zhifanjia <allenjia98@gmail.com>
2024-11-29 14:51:21 -0500

    adjust static file

--- api/index.js
+++ api/index.js
@@ -34,9 +34,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 app.use((err, req, res, next) => {
